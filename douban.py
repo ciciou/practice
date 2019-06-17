8e6b3d6b6b0ae93911cf5a9ea3f1041f89ea8475
ciciou <2207679962@qq.com>
2019-06-17 16:43:59 +0800

    将爬取的数据存入mysql数据库

--- douban.py
+++ douban.py
@@ -1,42 +1,67 @@
 import requests
+import pymysql
 from lxml import html
-k=1
-#请求网页内容
-#获取分页
-#for循环构造链接翻页
+k = 1
 for i in range(10):
-    #请求网页地址
-    url='https://movie.douban.com/top250?start={:d}&filter='.format(i*25)
-    #获取网页的源代码
-    con=requests.get(url).content
-    #调用html.fromstring函数解析html的源代码
-    sel=html.fromstring(con)
-    #所有的信息都在class属性为info的div标签里，可以先把这个节点取出来//*[@id="content"]/div/div[1]/ol
+    url = 'https://movie.douban.com/top250?start={}&filter='.format(i*25)
+    con = requests.get(url).content
+    sel = html.fromstring(con)
+    # 所有的信息都在class属性为info的div标签里，可以先把这个节点取出来 //*[@id="content"]/div/div[1]/ol
     for i in sel.xpath('//div[@class="info"]'):
-        #影片名称
-        title=i.xpath('div[@class="hd"]/a/span[@class="title"]/text()')[0]
-        #print(title)
-        info=i.xpath('div[@class="bd"]/p[1]/text()')
-        #导演演员信息
-        info_1=info[0].replace(" ", "").replace("\n", "")
-        #上映日期
-        date=info[1].replace(" ", "").replace("\n", "").split("/")[0]
-        #制片国家
-        country=info[1].replace(" ", "").replace("\n", "").split("/")[1]
-        #影片类型
-        geners=info[1].replace(" ", "").replace("\n", "").split("/")[2]
-        #评分
-        rate=i.xpath('//span[@class="rating_num"]/text()')[0]
-        #评论人数
-        comCount=i.xpath('//div[@class="star"]/span[4]/text()')[0]
-        #评论
-        comment = i.xpath('//div[@class="bd"]/p[2][@class="quote"]/span[@class="inq"]/text()')
-        #打印结果
-        print("TOP{}" .format(k))
-        print(title, info_1, rate, date, country, geners, comCount, comment)
-        with open("TOP250.txt", "a", encoding='utf-8') as f:
-            f.write(
-                "TOP%s\n影片名称：%s\n评分：%s %s\n上映日期：%s\n上映国家：%s\n评论：%s\n导演： %s\n" % (k, title, rate, comCount, date, country, comment, info_1))
-            f.write("=========================================\n")
 
-        k+=1
+        # 影片名称
+        title = i.xpath('div[@class="hd"]/a/span[@class="title"]/text()')[0]
+        #print(title)
+        info = i.xpath('div[@class="bd"]/p[1]/text()')
+        # 导演演员信息
+        info_1 = info[0].replace(" ", "").replace("\n", "")
+        # 上映日期
+        date = info[1].replace(" ", "").replace("\n", "").split("/")[0]
+        # 制片国家
+        country = info[1].replace(" ", "").replace("\n", "").split("/")[1]
+        # 影片类型
+        geners = info[1].replace(" ", "").replace("\n", "").split("/")[2]
+        # 评分
+        rate = i.xpath('//span[@class="rating_num"]/text()')[0]
+        # 评论人数
+        comCount = i.xpath('//div[@class="star"]/span[4]/text()')[0]
+
+        # 打印结果看看
+        print ("TOP%s" % str(k))
+        print( title, info_1, rate, date, country, geners, comCount )
+
+
+        connection=''
+        try:
+            # 获取一个有效的数据库连接对象，此处填写你的数据库信息，特别注意charset一定要写成'utf8'，不能写成'utf-8'。
+            connection = pymysql.connect(host='localhost', port=3306,
+                                         user='root', password='lzy6352341',
+                                         db='crawl', charset='utf8')
+            if connection:
+                print("[mysql]>>正确获取数据库的连接对象")
+
+            # 创建一个游标对象
+            cursor = connection.cursor()
+            print('[mysql]正确获取游标对象')
+            # 设置插入数据的sql语句模板
+            sql = "insert into doubanmovie VALUES (null,'%d','%s','%s,','%s','%s','%s','%s')" % (k, title, rate, comCount, date, country, info_1)
+            print('[mysql]>>%s' % sql)
+
+            # 使用游标对象发送sql语句并将服务器结果返回
+            affectedRows = cursor.execute(sql)
+            msg = '[mysql]>>写入操作成功' if affectedRows > 0 else '[mysql]>>写入失败'
+            print(msg)
+            # 事务提交
+            connection.commit()
+            print("[mysql]>>事务提交")
+        except Exception as e:
+            connection.rollback()
+            print('[mysql]事务回滚', format(e))
+        finally:
+            # 关闭数据库连接
+            connection.close()
+            print("[mysql]>>关闭数据库连接")
+            k += 1
+
+
+
